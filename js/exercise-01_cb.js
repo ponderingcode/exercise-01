2c5ca2a38b06cd0d7278c1edb9bd8d3a42c37068
Ryan Fleming <ryan@ponderingcode.com>
2016-07-14 13:13:13 -0400

    creating repo

---
+++ js/exercise-01_cb.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    
+});
+
+var val0;
+var val1;
+var val2;
+var val3;
+var val4;
+
+var floor;
+var ceiling;
+var mean;
+var sum;
+var product;
+
+function determineLeast(val) {
+    
+}
